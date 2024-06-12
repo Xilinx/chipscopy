bd80ea973133db5dca3843104125096ecac762dd
Mike <41343262+mikexilinx@users.noreply.github.com>
2024-06-12 14:03:40 -0700

    Transfer open-source files from server

--- 2024.1/_static/documentation_options.js
+++ 2024.1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2024.1.1717698244',
+    VERSION: '2024.1.1717799899',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
