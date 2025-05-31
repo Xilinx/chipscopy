f24b63c48bc2f7d158edf174721e71ae1daccc83
Mike <41343262+mikexilinx@users.noreply.github.com>
2025-05-30 18:18:59 -0700

    2025.1 - Major Release - CR-1239400 - Add program set to chipscopy memory example - CR-1241135 - Clarify ILA sample data usage - CR-1240247 - Report correct bandwidth numbers for DDRMC5E element - CR-1234943 - Add storage for crypto node - CR-1185161 - Add error_status to the device top level properties - CR-1220321 - Fix image display for DDR 2D eyescan example - CR-1185161 - Add error_status to device top level properties - CR-1240155 - Fix querylist compatibility with more-itertools 1.7.0 - Fix lin64 install issue due to pywinpty cargo requirement - Fix sphinx errors and warnings during doc generation - vhk158 and noc perfmon example changes - Add IBERT vck190-vcu128 heterogeneous cross-link-debug example - Allow delay_after_program for non-progress bar cases - Fix GTM link detect test - Adding efuse tcf commands to proxy - Auto link detection API support for IBERT - stimgen: Adding por target arg - Allow progress and done callbacks from API layer - Adding XicomFlashProxy, stimgen support doc, and image ids for detectHub - YK Scan: Add an optional stop callback - Python 3.13 compatibility changes - Enhanced example design finder to fallback to site-packages area - Adding source files for vcu128 board related to example_ibert_vcu128_q131_q132_q134_q135.bit

--- 2025.1/_static/documentation_options.js
+++ 2025.1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2025.1.1747162617',
+    VERSION: '2025.1.1748564032',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
